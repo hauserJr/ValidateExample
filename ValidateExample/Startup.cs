4ad236207f333399df053a71001e34f82ec9a131
free5 <free5@HAUSER-PC>
2017-02-22 01:38:12 +0800

    加入專案檔案。

---
+++ ValidateExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ValidateExample.Startup))]
+namespace ValidateExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
